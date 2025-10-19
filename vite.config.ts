6dda5b55ef10b0b681e6b18103951998df1eb1f7
张博 <uncle1bo@foxmail.com>
2025-10-19 13:20:56 +0800

    change: 输出路由方便调试

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,14 @@ import { cloudflare } from "@cloudflare/vite-plugin";
 import Pages from "vite-plugin-pages"
 
 export default defineConfig({
-  plugins: [react(), cloudflare(), Pages({ dirs: ['src/react-app/pages'] })],
+  plugins: [
+    react(),
+    cloudflare(),
+    Pages({
+      dirs: ['src/react-app/pages'],
+      onRoutesGenerated(routes) {   // ← 加这一行
+        console.log('\n[Pages] 生成路由：')
+        console.log(JSON.stringify(routes, null, 2))
+      },
+    })],
 });
